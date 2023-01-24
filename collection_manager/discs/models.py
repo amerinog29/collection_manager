e77704dc9d82390707a1f9addf2e13f31aa8f645
amerino-itic <antonio@itic.com.pe>
2023-01-24 18:35:09 -0500

    Se crearon los modelos para el app de discos.

--- collection_manager/discs/models.py
+++ collection_manager/discs/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Album(models.Model):
+	name = models.CharField(max_length=100)
+	release_date = models.DateField()
+
+
+class Musician(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='album')
+
+
+class Observations(models.Model):
+	text = models.TextField()
+	date = models.DateTimeField(auto_now=True)
+	
+	
+class Genre(models.Model):
+	name = models.CharField(max_length=150)
+
+
+class Disc(models.Model):
+	artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+	country = models.CharField(max_length=20)
+	observation = models.ForeignKey(Observations, on_delete=models.CASCADE)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
