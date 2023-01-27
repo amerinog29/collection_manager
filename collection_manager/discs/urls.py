261b3aab49896fb17751c87bb572f0899897af0b
amerino-itic <antonio@itic.com.pe>
2023-01-27 00:32:27 -0500

    Se agregaron las rutas para listar y crear artistas.

--- collection_manager/discs/urls.py
+++ collection_manager/discs/urls.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import test
+from .views import *
 
 collectionpatterns = []
 
-musicianpatterns = []
+musicianpatterns = [
+    path('listar', artist_list, name='artist_list'),
+    path('crear', create_artist, name='create_artist'),
+]
 
 albumpatterns = []
 
 genrepatterns = []
 
 urlpatterns = [
-    path('', test, name='test'),
+    # path('', test, name='test'),
     path('coleccion/', include(collectionpatterns)),
     path('artista/', include(musicianpatterns)),
     path('album/', include(albumpatterns)),
