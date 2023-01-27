05ff7e07067308cd0c6849f0180c8f4cb7888ec8
amerino-itic <antonio@itic.com.pe>
2023-01-27 00:46:07 -0500

    Se crearon las funciones para listar y crear artistas.

--- collection_manager/discs/views.py
+++ collection_manager/discs/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Artist
 
 
-def test(request):
-	return render(request, 'base.html')
+def artist_list(request):
+	artists = Artist.objects.all()
+	context = {'artists': artists}
+	return render(request, 'artist/artist_list.html', context)
+
+
+def create_artist(request):
+	if request.method == 'POST':
+		print(request.POST)
+		print(request.FILES)
+		Artist.objects.create(logo=request.FILES.get('logo'), name=request.POST.get('name'))
+		
+		return redirect('artist_list')
+	
+	return render(request, 'artist/create_artist.html')
