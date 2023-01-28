a02a2de94dcabcb859d0268e205d08d75761bbd7
amerino-itic <antonio@itic.com.pe>
2023-01-28 02:58:44 -0500

    Se crearon las funciones de edición y eliminación de un artista.

--- collection_manager/discs/views.py
+++ collection_manager/discs/views.py
@@ -7,15 +7,45 @@ from .models import Artist
 def artist_list(request):
 	artists = Artist.objects.all()
 	context = {'artists': artists}
+	
 	return render(request, 'artist/artist_list.html', context)
 
 
 def create_artist(request):
 	if request.method == 'POST':
-		print(request.POST)
-		print(request.FILES)
 		Artist.objects.create(logo=request.FILES.get('logo'), name=request.POST.get('name'))
 		
 		return redirect('artist_list')
 	
 	return render(request, 'artist/create_artist.html')
+
+
+def edit_artist(request, pk):
+	if request.method == 'POST':
+		if 'photo' in request.FILES:
+			defaults = {'photo': request.FILES.get('photo'), 'name': request.POST.get('name')}
+		else:
+			defaults = {'name': request.POST.get('name')}
+			
+		Artist.objects.update_or_create(pk=pk, defaults=defaults)
+		
+		return redirect('artist_list')
+	
+	else:
+		artist = Artist.objects.get(pk=pk)
+	
+	return render(request, 'artist/create_artist.html', {'artist': artist})
+
+
+def delete_artist(request, pk):
+	try:
+		artist = Artist.objects.get(pk=pk)
+		artist.delete()
+		
+		return redirect('artist_list')
+		
+	except Artist.DoesNotExist:
+		return redirect('artist_list')
+	
+		
+		
