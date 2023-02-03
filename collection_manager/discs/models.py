d2e71a883ddc1c9803320fbea9212ec4d51e1377
amerino-itic <antonio@itic.com.pe>
2023-02-03 18:51:44 -0500

    Se agregaron imágenes por defecto en los campos ImageField. Se modificó completamente el modelo de Disc agrupando campos con album.

--- collection_manager/discs/models.py
+++ collection_manager/discs/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class Artist(models.Model):
-	photo = models.ImageField(upload_to='photos')
+	photo = models.ImageField(upload_to='photos', default='disc/default_avatar.png')
 	name = models.CharField(max_length=150)
 	
 	
 class Album(models.Model):
-	photo = models.ImageField(upload_to='album')
+	photo = models.ImageField(upload_to='album', default='default_album.jpg')
 	name = models.CharField(max_length=100)
 	release_date = models.DateField()
 	artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='artist')
@@ -25,7 +25,10 @@ class Genre(models.Model):
 
 
 class Disc(models.Model):
+	photo = models.ImageField(upload_to='disc', default='disc/default_vinyl.png')
+	name = models.CharField(max_length=100)
 	artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-	country = models.CharField(max_length=20)
-	observation = models.ForeignKey(Observation, on_delete=models.CASCADE)
-	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+	country = models.CharField(max_length=20, null=True, blank=True)
+	release_date = models.DateField(null=True)
+	observation = models.ForeignKey(Observation, on_delete=models.CASCADE, null=True)
+	genre = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True)
