5218816faca23db48a265301c959165c998c3d3c
amerino-itic <antonio@itic.com.pe>
2023-03-05 21:20:25 -0500

    Se agregó la ruta para iniciar sesión.

--- collection_manager/collection_manager/urls.py
+++ collection_manager/collection_manager/urls.py
@@ -18,8 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+from discs.views import sign_in
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', sign_in, name='sign_in'),
     path('discos/', include('discs.urls')),
 ]
 
